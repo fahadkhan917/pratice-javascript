e305c3ef0a6e84787dd422ef77727ac8391a5b13
fahadkhan917 <fk16181@gmail.com>
2020-07-16 22:46:07 +1000

    Update app.js

--- app.js
+++ app.js
@@ -13,22 +13,30 @@
 // }
 
 
-var arr=[12,48,45,56,88];
-var hi=0;
-for(var i=0;i<5;i++){
-if(arr[i]>hi){
+// var arr=[12,48,45,56,88];
+// var hi=" ";
+// for(var i=0;i<5;i++){
+// if(arr[i]>hi){
 
-hi=arr[i];
+// hi=arr[i];
 
-}
-
-
-
-}
+// }
 
 
-document.write(hi);
 
+// }
+
+
+// document.write(hi);
+
+
+var arr=["SCC","HSE","BCS","BS","BCOM","MS","M.PHIL","PHD"];
+document.write(arr[0]+"<br>");
+document.write(arr[1]);
+document.write(arr[2]);
+document.write(arr[3]);
+document.write(arr[4]);
+document.write(arr[5]);
 
 
 
