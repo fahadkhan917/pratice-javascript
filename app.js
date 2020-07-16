0e2a983e23b89ed2858cb40bdb467496bcedf979
fahadkhan917 <fk16181@gmail.com>
2020-07-16 22:12:36 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 
 
-var a=prompt("enter the name");
+// var a=prompt("enter the name");
+// var chk="";
+// for(var i=4;i>=0;i--){
+// chk+=a[i];
+
+
+// }
+// if(chk===a){
+
+//     document.write("this is word");
+// }
+
+
+var arr=[12,48,45,56,88];
+var hi=0;
+for(var i=0;i<5;i++){
+if(arr[i]>hi){
+
+hi=arr[i];
+
+}
+
+
+
+}
+
+
+document.write(hi);
+
+
+
+
